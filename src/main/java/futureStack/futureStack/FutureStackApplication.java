2af89b431598fe72e0682e948916c13bcfd186b3
MaryChriss <marychris.crf@gmail.com>
2025-11-10 19:47:16 -0300

    init

---
+++ src/main/java/futureStack/futureStack/FutureStackApplication.java
@@ -0,0 +1,13 @@
+package futureStack.futureStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/futureStack/futureStack/FutureStackApplicationTests.java
@@ -0,0 +1,13 @@
+package futureStack.futureStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
